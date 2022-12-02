387af9e596ebe609f21bfa4663f08d54ff4be97f
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-12-02 22:03:49 +0530

    Reverse The Given Number

---
+++ ReverseTheNumber.cs
@@ -0,0 +1,23 @@
+﻿namespace Reverse_The_Number
+{
+    class ReverseTheNumber
+    {
+        static void Main(string[] args)
+        {
+            int Number, Reverse = 0, Rem;
+            Console.WriteLine("Enter a Number To Reverse :");
+            Number = int.Parse(Console.ReadLine());
+
+            while (Number != 0)
+            {
+                Rem = Number % 10;
+                Reverse = Reverse * 10 + Rem;
+                Number /= 10;
+            }
+            Console.WriteLine("Reversed Number: "+ Reverse);
+            Console.ReadLine();
+
+
+        }
+    }
+}
